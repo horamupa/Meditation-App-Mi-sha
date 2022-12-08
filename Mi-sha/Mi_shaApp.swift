0b9c78a3f771576c32d5aa7d7d343e51baea7862
horamupa <horamupa@gmail.com>
2022-12-08 10:32:57 +0400

    Initial Commit

---
+++ Mi-sha/Mi_shaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mi_shaApp.swift
+//  Mi-sha
+//
+//  Created by MM on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Mi_shaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
