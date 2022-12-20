53ffc56b3a5877edf5c1ef081ed6c1e7fd8f38d3
horamupa <horamupa@gmail.com>
2022-12-20 16:21:39 +0400

    minor fixes

--- Mi-sha/Core/HomeScreen/HomeView.swift
+++ Mi-sha/Core/HomeScreen/HomeView.swift
@@ -21,97 +21,26 @@ struct HomeView: View {
 //            Color.theme.black
             Color.theme.white
                 .ignoresSafeArea()
-            VStack(alignment: .leading) {
-  
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Good")
-                        Text("Morning")
-                    }
-                    .foregroundColor(Color.theme.black)
-                    .font(.meditation(.bold, size: 40))
-                    
-                    Spacer()
-                    NavigationLink {
-                        LoginView()
-                    } label: {
-                        Image(systemName: "text.justify.left")
-                            .font(.system(size: 30, weight: .bold))
-                            .frame(width: 50, height: 80, alignment: .topTrailing)
-                    }
-                }
-                .frame(maxHeight: .infinity, alignment: .topLeading)
-                
+            VStack(alignment: .leading, spacing: 15) {
+                Spacer()
+               upperBlock
                 Text("Have a great day!")
                     .font(.labGrotesque(.regular, size: 18))
+               middleBlock
+               
+                Spacer()
                 
-                Rectangle()
-                    .frame(height: 250)
-                    .foregroundColor(Color.theme.orange)
-                    
-                    .overlay(alignment: .bottomTrailing) {
-                        HStack {
-                            Text("Start your journey here")
-                                .font(.labGrotesque(.medium, size: 16))
-                                Spacer()
-                            Image(systemName: "play.circle")
-                                .scaleEffect(2.5)
-                                .foregroundColor(Color.theme.white)
-                                .frame(width: 50, height: 50)
-                        }
-                        .padding(5)
-                        .padding(.horizontal, 20)
-                        .frame(maxWidth: .infinity)
-                        .background(.ultraThinMaterial)
-                        
-                    }
-                    .cornerRadius(25)
-                
-                    Text("Explore yourself")
+               Text("Выбери свой путь")
                     .font(.labGrotesque(.medium, size: 20))
                    
-                ScrollView(.horizontal) {
-                    HStack(spacing: 5) {
-                        if !vm.courses.isEmpty {
-                            ForEach(vm.courses) { item in
-                                NavigationLink(destination: CourseView(innerCourse: item)) {
-                                    ZStack(alignment: .bottomTrailing) {
-                                        Rectangle()
-                                            .foregroundColor(Color(item.image))
-                                        
-                                        Text(item.name)
-                                            .font(.labGrotesque(.regular, size: 16))
-                                            .minimumScaleFactor(0.5)
-                                            .padding(15)
-                                            .frame(maxWidth: .infinity)
-                                            .background(.ultraThinMaterial)
-                                            
-                                    }
-                                    .frame(width: UIScreen.main.bounds.width / 2.5, height: 250)
-                                    .cornerRadius(25)
-                                }
-                            }
-                        } else {
-                                                Rectangle()
-                                                    .frame(width: UIScreen.main.bounds.width / 2.5, height: 250)
-                                                    .foregroundColor(Color.theme.blue)
-                                                    .cornerRadius(25)
-                            
-                                                Spacer()
-                                                Rectangle()
-                                                    .frame(width: UIScreen.main.bounds.width / 2.5, height: 250)
-                                                    .foregroundColor(Color.theme.purple)
-                                                    .cornerRadius(25)
-                        }
-                    }
-                    .frame(maxWidth: .infinity)
-                }
+               bottomBlock
             }
             .padding(30)
             .foregroundColor(Color.theme.black)
             .navigationBarHidden(true)
+//            .ignoresSafeArea()
         }
-       
+//        .ignoresSafeArea()
         .sheet(isPresented: $isSettings, content: {
             SettingsView()
             
@@ -130,6 +59,97 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 extension HomeView {
+    
+    var upperBlock: some View {
+//        Group {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Good")
+                    Text("Morning")
+                }
+                .foregroundColor(Color.theme.black)
+                .font(.meditation(.bold, size: 40))
+                
+                
+                Spacer()
+                NavigationLink {
+                    SettingsView()
+                } label: {
+                    Image(systemName: "text.justify.left")
+                        .font(.system(size: 30, weight: .bold))
+                        .frame(width: 50, height: 80, alignment: .topTrailing)
+                }
+            }
+        
+//            .frame(maxHeight: .infinity, alignment: .topLeading)
+        
+            
+          
+//        }
+    }
+    
+    var middleBlock: some View {
+        Rectangle()
+            .frame(height: 250)
+            .foregroundColor(Color.theme.orange)
+            
+            .overlay(alignment: .bottomTrailing) {
+                HStack {
+                    Text("Start your journey here")
+                        .font(.labGrotesque(.medium, size: 16))
+                        Spacer()
+                    Image(systemName: "play.circle")
+                        .scaleEffect(2.5)
+                        .foregroundColor(Color.theme.white)
+                        .frame(width: 50, height: 50)
+                }
+                .padding(5)
+                .padding(.horizontal, 20)
+                .frame(maxWidth: .infinity)
+                .background(.ultraThinMaterial)
+                
+            }
+            .cornerRadius(25)
+    }
+    
+    var bottomBlock: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 20) {
+                if !vm.courses.isEmpty {
+                    ForEach(vm.courses) { item in
+                        NavigationLink(destination: CourseView(innerCourse: item)) {
+                            ZStack(alignment: .bottomTrailing) {
+                                Rectangle()
+                                    .foregroundColor(Color(item.image))
+                                
+                                Text(item.name)
+                                    .font(.labGrotesque(.regular, size: 16))
+                                    .minimumScaleFactor(0.5)
+                                    .padding(15)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
+                                    
+                            }
+                            .frame(width: UIScreen.main.bounds.width / 2.5, height: 250)
+                            .cornerRadius(25)
+                        }
+                    }
+                } else {
+                                        Rectangle()
+                                            .frame(width: UIScreen.main.bounds.width / 2.5, height: 250)
+                                            .foregroundColor(Color.theme.blue)
+                                            .cornerRadius(25)
+                    
+//                                                Spacer()
+                                        Rectangle()
+                                            .frame(width: UIScreen.main.bounds.width / 2.5, height: 250)
+                                            .foregroundColor(Color.theme.purple)
+                                            .cornerRadius(25)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
         
         var halfScreen: CGFloat {
             UIScreen.main.bounds.width / 2
